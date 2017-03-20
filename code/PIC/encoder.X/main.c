f912915668ab08ff7a7ad0b42e5dd18fe1ae2d31
Mathieu David <mathieudavid@mathieudavid.org>
2017-03-21 00:05:21 +0100

    Début de code PIC pour les encodeurs

---
+++ code/PIC/encoder.X/main.c
@@ -0,0 +1,99 @@
+/*
+ * File:   main.c
+ * Author: Mathieu David
+ *
+ * Created on March 20, 2017, 7:02 PM
+ */
+
+
+#include <xc.h>
+
+// ----------------------
+// Hardware Configuration
+// ----------------------
+// Datasheet p. 268
+
+// CONFIG1H
+#pragma config OSC = HSPLL      // Oscillator Selection bits (HS oscillator, PLL enabled (clock frequency = 4 x FOSC1))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal External Oscillator Switchover bit (Internal External Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRTEN = OFF     // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled)
+// BORV = No Setting
+
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDPS = 32768     // Watchdog Timer Postscale Select bits (1:32768)
+#pragma config WINEN = OFF      // Watchdog Timer Window Enable bit (WDT window disabled)
+
+// CONFIG3L
+#pragma config PWMPIN = OFF     // PWM output pins Reset state control (PWM outputs disabled upon Reset (default))
+#pragma config LPOL = HIGH      // Low-Side Transistors Polarity (PWM0, 2, 4 and 6 are active-high)
+#pragma config HPOL = HIGH      // High-Side Transistors Polarity (PWM1, 3, 5 and 7 are active-high)
+#pragma config T1OSCMX = ON     // Timer1 Oscillator MUX (Low-power Timer1 operation when microcontroller is in Sleep mode)
+
+// CONFIG3H
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (Enabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Low-Voltage ICSP Enable bit (Low-voltage ICSP disabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000200-000FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (001000-001FFF) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot Block (000000-0001FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000200-000FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (001000-001FFF) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0001FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000200-000FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (001000-001FFF) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from table reads executed in other blocks)
+
+
+// Function prototypes
+void interrupt low_priority low_isr(void);
+void interrupt high_isr(void);
+
+
+void main() {
+    // QEI module configuration
+    QEICONbits.VELM = 0; // Velocity mode disabled
+    QEICONbits.ERROR = 0; // Disable overflow / underflow error
+    QEICONbits.QEIM = 0b101; // ?? I am not sure what to choose between 101 and 001
+    
+    
+    while (1) {
+
+    }
+}
+
+// -----------------------
+// Interrupt routines
+// -----------------------
+
+// High priority interrupt
+void interrupt high_isr (void)
+{
+
+}
+// Low priority interrupt
+void interrupt low_priority low_isr (void)
+{
+    
+}
